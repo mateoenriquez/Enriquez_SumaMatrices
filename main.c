ba75561b02cd3b6fe3798a656c1f165a1d90b883
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-11-28 14:55:47 -0500

    Numeros aleatorios en matrices

--- main.c
+++ main.c
@@ -18,14 +18,54 @@ int main(){
     scanf("%d", &m2);
 
     int matriz[n][m];
-    int matriz[n2][m2];
+    int matriz2[n2][m2];
 
     if (n=!n2 || m=!m2)
     {
         printf("La suma no puede ser dada por las matrices no son iguales entre si \n");
         return 0;
-    }    
+    }
 
+        for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matriz[i][j]=rand()%100+1;
+        }
+        
+    }
+
+        for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", matriz[i][j])
+        }
+        
+    }
+    
+
+        for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matriz2[i][j]=rand()%100+1;
+        }
+        
+    }
+
+        for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", matriz2[i][j])
+        }
+        
+    }
+    
+            
 
     return 0;
 }
