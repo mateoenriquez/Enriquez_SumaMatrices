4b91e589eabe5c0d630f6f2ace1e137d8ca2e449
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-11-28 14:52:25 -0500

    Introducion de filas y columnas por el usuario y matrices inicializadas

--- main.c
+++ main.c
@@ -8,5 +8,20 @@ int main(){
     int n2, m2;
     srand(time(NULL));
 
+    printf("Ingresar el numero de filas de la primera matriz: ");
+    scanf("%d", &n);
+    printf("Ingresar el numero de columnas de la primera matriz: ");
+    scanf("%d", &m);
+    printf("Ingresar el numero de filas de la segunda matriz: ");
+    scanf("%d", &n2);
+    printf("Ingresar el numero de columnas de la segunda matriz: ");
+    scanf("%d", &m2);
+
+    int matriz[n][m];
+    int matriz[n2][m2];
+
+    
+
+
     return 0;
 }
