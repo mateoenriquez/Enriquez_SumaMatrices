6643d36f102e5b120f5abc4915b71baf8c0026ed
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-11-29 09:28:59 -0500

    Comentarios al codigo añadidos

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+//Se ingresan las librerias necesarias para el codigo
+
 int main(){
 
     int n, m;
     int n2, m2;
     int n3, m3;
     srand(time(NULL));
+
+    //Se inicializan las respetivas variables para las matrices a sumar
+    //Se pide al usuario ingresar el numero de filas y columnas de cada una de las matrices
 
     printf("Ingresar el numero de filas de la primera matriz: ");
     scanf("%d", &n);
@@ -20,13 +25,18 @@ int main(){
 
     int matriz[n][m];
     int matriz2[n2][m2];
-    
 
+    //Se inicializan las dos matrices con los datos dados por el usuario
+    
     if (n!=n2 || m!=m2)
     {
         printf("La suma no puede ser dada por las matrices no son iguales entre si \n");
         return 0;
     }
+
+    //Se ingresa un condicional donde si el numero de filas o columnas no es el mismo entre matrices
+    //no se ejecute el codigo, esto debido a que para una suma de matrices ambas deben de tener las mismas
+    //dimensiones
 
     n3=n2=n;
     m3=n2=n;
@@ -51,8 +61,9 @@ int main(){
         }
         
     }
-    
 
+    //Se asignan entradas con numeros aleatorios del 1 al 100 a la matriz y se la escribe
+    
         for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -73,6 +84,8 @@ int main(){
         
     }
 
+    //Se asigna de igual forma entradas con numeros aleatorios a la segunda matriz y se escribe la misma.
+
         for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -91,7 +104,10 @@ int main(){
             printf("%d ", matriz3[i][j]);
         }
         
-    }    
+    }
+
+    //Por ultimo se suman ambas matrices en una matriz 3 la cual posteriormente se escribe como la totalidad de la suma
+    //entre matriz 1 y 2    
 
     return 0;
 }
